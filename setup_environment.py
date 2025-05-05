8332e22badc0ebceb1eaf0dc3a634eab2d5c2c61
Tiziano Pacifico <gourmetmolise@gmail.com>
2025-05-05 15:25:05 +0200

    pdf tools

--- setup_environment.py
+++ setup_environment.py
@@ -13,5 +13,6 @@ def set_environment_variables(project_name: str = "") -> None:
     os.environ["LANGCHAIN_TRACING_V2"] = "true"
     os.environ["LANGCHAIN_API_KEY"] = str(config("LANGCHAIN_API_KEY"))
     os.environ["LANGCHAIN_PROJECT"] = project_name
+    os.environ["TAVILY_API_KEY"] = str(config("TAVILY_API_KEY"))
 
     print("API Keys loaded and tracing set with project name: ", project_name)
